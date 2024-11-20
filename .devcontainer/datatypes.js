76e719760bb4f6f8951f782f4a6133cb7016e730
Naman1002 <58694695+Naman1002@users.noreply.github.com>
2024-11-20 13:38:14 +0000

    config changes done

---
+++ .devcontainer/datatypes.js
@@ -0,0 +1,14 @@
+"use strict" // treats the JS code as a new JS code
+
+let name = "Naman"
+let age =3
+let isLoggedIn = false
+
+console.log(typeof "Hitesh") //string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof age); //number
+
+//"33" => 33
+//"33abc" => NaN
+// true =>1, false =>0 
---
+++ .devcontainer/variables_const.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "abc@gmail.com"
+var accountPassword = "abcdef"
+accountCity = "Jaipur"
+let accountState;
+
+/*
+Prefer not to use Var, because of an issue in block scope and functional scope 
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
