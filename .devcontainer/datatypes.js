69e53ed5d06c5870b068a25229a47b5e71e16a80
Naman1002 <58694695+Naman1002@users.noreply.github.com>
2024-11-21 05:19:48 +0000

    dataypes summary

--- .devcontainer/datatypes.js
+++ .devcontainer/datatypes.js
@@ -4,11 +4,23 @@ let name = "Naman"
 let age =3
 let isLoggedIn = false
 
-console.log(typeof "Hitesh") //string
-console.log(typeof null); // object
-console.log(typeof undefined); // undefined
-console.log(typeof age); //number
+//console.log(typeof "Hitesh") //string
+//console.log(typeof null); // object
+//console.log(typeof undefined); // undefined
+//console.log(typeof age); //number
 
 //"33" => 33
 //"33abc" => NaN
 // true =>1, false =>0 
+
+// Operations
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue)
+
+console.log( null > 0);
+console.log( null == 0);
+console.log( null >= 0);
+
+//=== strict operators 
---
+++ .devcontainer/datatypes_2.js
@@ -0,0 +1,25 @@
+// Primitive
+
+//7 types : String, Number, Boolean, null, Undefined, Symbol,
+//BigInt
+
+const score = 100
+const score_value = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null;
+let userEmail;
+const id = Symbol('123');
+
+
+// Reference(Non Primitive):
+// Arrays, Objects, Functions
+
+const heroes = ["shaktiman","Nagraj","Doga"]
+const myObj  = {
+    name : "Xyz",
+    age:999
+}
+const functionality = function name(params) {
+    console.log("Hello World")
+}
