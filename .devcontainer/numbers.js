3ae566076cfd590c5567a92606e87ff733302458
Naman1002 <58694695+Naman1002@users.noreply.github.com>
2024-11-24 15:26:22 +0000

    numbers lecture completed

---
+++ .devcontainer/numbers.js
@@ -0,0 +1,29 @@
+const score = 400;
+const balance = new Number(400);
+
+// console.log(balance);
+// console.log(balance.toString.length);
+// console.log(balance.toFixed(2));
+
+const theNumber = 1234.907
+//console.log(theNumber.toPrecision(4))
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+///////////////////////// Math /////////////////////////////
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random())
+
+max = 15
+min =10
+console.log((Math.random()*10) + 1);
+console.log(Math.random()*( max - min +1) + min); 
